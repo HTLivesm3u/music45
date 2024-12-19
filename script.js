09c3a5439ca06696d215f20b36f17780726db9d2
HTLivesm3u <169054920+HTLivesm3u@users.noreply.github.com>
2024-12-19 22:33:10 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,23 +1,28 @@
 // Song playlist data
 const playlist = [
+     { 
+      title: "Jo Tum Mere Ho",
+    artist: "Anuv Jain",
+    src: "music/Jo Tum Mere Ho.mp3",
+    cover: "https://c.saavncdn.com/401/Jo-Tum-Mere-Ho-Hindi-2024-20240731053953-500x500.jpg",
+     },
   {
-    title: "Song Title 1",
-    artist: "Artist Name 1",
-    src: "song1.mp3",
-    cover: "cover1.jpg",
+    title: "ekati ekati ghabarlis na",
+    artist: "Anjali Kulkarni",
+    src: "music/song1.mp3",
+    cover: "https://imgs.search.brave.com/HVqjF7CWQpUU0UGZZaMtr67XIyOoIGuUrRJuGBv8z8I/rs:fit:860:0:0:0/g:ce/aHR0cHM6Ly9tLm1l/ZGlhLWFtYXpvbi5j/b20vaW1hZ2VzL00v/TVY1QllUaGlaV015/WmpRdFlUQXhaUzAw/TkdNMkxUZ3hOREF0/Tnpnd05HRmhOakUx/WkRJNVhrRXlYa0Zx/Y0djQC5qcGc",
   },
   {
-    title: "Song Title 2",
-    artist: "Artist Name 2",
-    src: "song2.mp3",
-    cover: "cover2.jpg",
+    title: "AAI",
+    artist: "Yogita Koli,Pravin Koli",
+    src: "music/song2.mp3",
+    cover: "https://c.saavncdn.com/621/Aai-Marathi-2022-20220505134712-500x500.jpg",
   },
   {
-    title: "Song Title 3",
-    artist: "Artist Name 3",
-    src: "song3.mp3",
-    cover: "cover3.jpg",
-  },
+    title: "govyachya kinaryavar",
+    artist: "pravin koli",
+    src: "music/song3.mp3",
+    cover: "music/song33.jpg",},
 ];
 
 // Elements

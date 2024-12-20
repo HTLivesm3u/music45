fdf45533b13e50dc4e0478e49e77572c3e04cf3e
HTLivesm3u <169054920+HTLivesm3u@users.noreply.github.com>
2024-12-20 11:56:04 +0530

    Update songs.js

--- songs.js
+++ songs.js
@@ -1,1 +1,22 @@
+// songs.js
+export const songs = [
+  {
+    title: "Song Title 1",
+    artist: "Artist Name 1",
+    src: "song1.mp3",
+    cover: "cover1.jpg",
+  },
+  {
+    title: "Song Title 2",
+    artist: "Artist Name 2",
+    src: "song2.mp3",
+    cover: "cover2.jpg",
+  },
+  {
+    title: "Song Title 3",
+    artist: "Artist Name 3",
+    src: "song3.mp3",
+    cover: "cover3.jpg",
+  },
+];
 

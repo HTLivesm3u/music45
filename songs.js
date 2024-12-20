8a430d0e6ed5ddfceaa6320906932823fbe3feeb
HTLivesm3u <169054920+HTLivesm3u@users.noreply.github.com>
2024-12-20 22:18:02 +0530

    Update songs.js

--- songs.js
+++ songs.js
@@ -12,6 +12,11 @@ export const songs = [
     src:"https://aac.saavncdn.com/815/483a6e118e8108cbb3e5cd8701674f32_96.mp4",
     cover:"https://c.saavncdn.com/815/Bhediya-Hindi-2023-20230927155213-500x500.jpg",
   },
+{ 
+  title: "Aankhon Mein Doob Jaane Ko",
+  artist: "Sandesh Shandilya",
+  src: "https://aac.saavncdn.com/730/b7bc5718c8eb6a4afcb7bd1f44d2c9ca_160.mp4",
+  cover: "https://c.saavncdn.com/730/Aankhon-Mein-Doob-Jaane-Ko-Hindi-2024-20240816183310-500x500.jpg" },
   {
     title: "ekati ekati ghabarlis na",
     artist: "Anjali Kulkarni",
@@ -29,11 +34,6 @@ export const songs = [
     artist: "pravin koli",
     src: "music/song3.mp3",
     cover: "music/song33.jpg",},
-{ 
-  title: "Aankhon Mein Doob Jaane Ko",
-  artist: "Sandesh Shandilya",
-  src: "https://aac.saavncdn.com/730/b7bc5718c8eb6a4afcb7bd1f44d2c9ca_160.mp4",
-  cover: "https://c.saavncdn.com/730/Aankhon-Mein-Doob-Jaane-Ko-Hindi-2024-20240816183310-500x500.jpg" },
 
 
 ];

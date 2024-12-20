a2d3a444222a323886ff448e49698e6d549a7b3b
HTLivesm3u <169054920+HTLivesm3u@users.noreply.github.com>
2024-12-20 22:28:14 +0530

    Update songs.js

--- songs.js
+++ songs.js
@@ -17,23 +17,30 @@ export const songs = [
   artist: "Sandesh Shandilya",
   src: "https://aac.saavncdn.com/730/b7bc5718c8eb6a4afcb7bd1f44d2c9ca_160.mp4",
   cover: "https://c.saavncdn.com/730/Aankhon-Mein-Doob-Jaane-Ko-Hindi-2024-20240816183310-500x500.jpg" },
+
   {
-    title: "ekati ekati ghabarlis na",
-    artist: "Anjali Kulkarni",
-    src: "music/song1.mp3",
-    cover: "https://c.saavncdn.com/875/Chintoo-Marathi-2012-20200122070309-500x500.jpg",
+    title:"Husn",
+    artist:"Anuv jain",
+    src:"https://aac.saavncdn.com/436/13795b7aa2e87393366162b9e6a6fe88_160.mp4",
+    cover:"https://c.saavncdn.com/436/Husn-Hindi-2023-20231129054140-500x500.jpg",
   },
-  {
-    title: "AAI",
-    artist: "Yogita Koli,Pravin Koli",
-    src: "music/song2.mp3",
-    cover: "https://c.saavncdn.com/621/Aai-Marathi-2022-20220505134712-500x500.jpg",
-  },
-  {
-    title: "govyachya kinaryavar",
-    artist: "pravin koli",
-    src: "music/song3.mp3",
-    cover: "music/song33.jpg",},
+  // {
+  //   title: "ekati ekati ghabarlis na",
+  //   artist: "Anjali Kulkarni",
+  //   src: "music/song1.mp3",
+  //   cover: "https://c.saavncdn.com/875/Chintoo-Marathi-2012-20200122070309-500x500.jpg",
+  // },
+  // {
+  //   title: "AAI",
+  //   artist: "Yogita Koli,Pravin Koli",
+  //   src: "music/song2.mp3",
+  //   cover: "https://c.saavncdn.com/621/Aai-Marathi-2022-20220505134712-500x500.jpg",
+  // },
+  // {
+  //   title: "govyachya kinaryavar",
+  //   artist: "pravin koli",
+  //   src: "music/song3.mp3",
+  //   cover: "music/song33.jpg",},
 
 
 ];

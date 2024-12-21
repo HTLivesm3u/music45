a40a25bd7d7020fdbef3cb898e24bb76f29e08c9
HTLivesm3u <169054920+HTLivesm3u@users.noreply.github.com>
2024-12-21 22:25:30 +0530

    Update songs.js

--- songs.js
+++ songs.js
@@ -29,12 +29,12 @@ export const hindiSongs = [
     src:"music/Chaleya (From  Jawan ) - Anirudh Ravichander, Arijit Singh, Shilpa Rao, Kumaar.m4a",
     cover:"https://c.saavncdn.com/026/Chaleya-From-Jawan-Hindi-2023-20230814014337-500x500.jpg",
   },
-  {
-    title:"",
-    artist:"",
-    src:"",
-    cover"",
-  },
+  // {
+  //   title:"",
+  //   artist:"",
+  //   src:"",
+  //   cover"",
+  // },
   
  ];
 

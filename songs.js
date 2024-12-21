2950d435241185626ee55755251dd4de0a86162b
HTLivesm3u <169054920+HTLivesm3u@users.noreply.github.com>
2024-12-21 22:55:13 +0530

    Update songs.js

--- songs.js
+++ songs.js
@@ -54,6 +54,61 @@ export const marathiSongs = [{
       src: "music/Aai - Pravin Koli, Yogita Koli, Deeya Wadkar, Myra Vaikul, Ankita Raut.m4a",
       cover: "https://c.saavncdn.com/621/Aai-Marathi-2022-20220505134712-500x500.jpg",
  },
+  {
+    title:"Jagnayache Deva",
+    artist:"?",
+    src:"music/Aise_Lago_Bhan_Full_Song_Jagnyache_Deva_Song_जगनयच_दव_Marathi.mp3",
+    cover:"https://i.pinimg.com/736x/0d/50/c7/0d50c7d6f608385fadcd964c3651eb92.jpg",
+  },
+  {
+    title:"Baharla Ha Madhumas",
+    artist:"Ajay-Atul, Ajay Gogavale, Shreya Ghoshal",
+    src:"music/Baharla Ha Madhumas - Ajay-Atul, Ajay Gogavale, Shreya Ghoshal.m4a",
+    cover:"https://c.saavncdn.com/989/Maharashtra-Shaheer-Marathi-2023-20230427192041-500x500.jpg",
+  },
+
+  {
+    title:"Chand Tu Nabhatla",
+    artist:"Swapnil Bandodkar",
+    src:"music/Chand Tu Nabhatla - Swapnil Bandodkar.m4a",
+    cover:"https://c.saavncdn.com/329/Sandook-Marathi-2015-500x500.jpg",
+  },
+  {
+    title:"Deva Tujhya Gaabhaaryaalaa",
+    artist:"Adarsh Shinde, Kirti Killedar, Anandi Joshi",
+    src:"music/Deva Tujhya Gaabhaaryaalaa - Adarsh Shinde, Kirti Killedar, Anandi Joshi.m4a",
+    cover:"https://c.saavncdn.com/708/Duniyadari-Marathi-2013-20241119160647-500x500.jpg",
+  },
+  {
+    title:"Dewak Kalji Re",
+    artist:"Ajay Gogavale",
+    src:"music/Dewak Kalji Re - Ajay Gogavale.m4a",
+    cover:"https://c.saavncdn.com/461/Redu-Marathi-2017-20180502-500x500.jpg",
+  }, 
+  {
+    title:"Dhaga Dhaga",
+    artist:"harsh wavre, Anandi Joshi",
+    src:"music/Dhaga Dhaga - harsh wavre, Anandi Joshi.m4a",
+    cover:"https://c.saavncdn.com/568/Dagdi-Chawl-Marathi-2015-20190610174048-500x500.jpg",
+  },  
+  {
+    title: "ekati ekati ghabarlis na",
+    artist: "Anjali Kulkarni",
+     src: "music/Ekati Ekati (feat  Shubhankar Kulkarni   Anjali Kulkarni).mp3",
+     cover: "https://c.saavncdn.com/875/Chintoo-Marathi-2012-20200122070309-500x500.jpg",
+   },
+  {
+    title:"Govyachya Kinaryav",
+    artist:"Shubhangii Kedar, Pravin Koli, Rajneesh Patel",
+    src:"music/Govyachya Kinaryav - Shubhangii Kedar, Pravin Koli, Rajneesh Patel.m4a",
+    cover:"https://c.saavncdn.com/122/Govyachya-Kinaryav-Marathi-2020-20210708114432-500x500.jpg",
+  }, 
+  {
+    title:"Gulabachi Kali",
+    artist:"Vaishali Samant, Urmila Dhangar, Amitraj",
+    src:"music/Gulabachi Kali - Vaishali Samant, Urmila Dhangar, Amitraj.m4a",
+    cover:"https://c.saavncdn.com/102/Tu-Hi-Re-Marathi-2015-20241119160647-500x500.jpg",
+  },                   
 ];
 
 
@@ -93,12 +148,7 @@ export const marathiSongs = [{
 //     cover:"https://c.saavncdn.com/111/Aalas-Ka-Pedh-Hindi-2015-20231005005130-500x500.jpg",
 //   },
   
-//   {
-//     title: "ekati ekati ghabarlis na",
-//     artist: "Anjali Kulkarni",
-//      src: "music/Husn - Anuv Jain.m4a",
-//      cover: "https://c.saavncdn.com/875/Chintoo-Marathi-2012-20200122070309-500x500.jpg",
-//    },
+
 //    {
 //     title: "AAI",
 //      artist: "Yogita Koli,Pravin Koli",

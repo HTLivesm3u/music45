a317b4076b03c11fd3c71aa7fefcad839e5cfef3
HTLivesm3u <169054920+HTLivesm3u@users.noreply.github.com>
2024-12-21 16:38:38 +0530

    Update songs.js

--- songs.js
+++ songs.js
@@ -1,65 +1,103 @@
 // songs.js
-export const songs = [
-  { 
-      title: "Jo Tum Mere Ho",
-    artist: "Anuv Jain",
-    src: "music/Jo Tum Mere Ho.mp3",
-    cover: "https://c.saavncdn.com/401/Jo-Tum-Mere-Ho-Hindi-2024-20240731053953-500x500.jpg",
-     },
+export const hindiSongs = [
   {
-    title: "Apna Bana Le",
-    artist: "Sachin-Jigar, Arijit Singh, Amitabh Bhattacharya",
-    src:"https://aac.saavncdn.com/815/483a6e118e8108cbb3e5cd8701674f32_96.mp4",
-    cover:"https://c.saavncdn.com/815/Bhediya-Hindi-2023-20230927155213-500x500.jpg",
+    title: "Tum Hi Ho",
+    artist: "Arijit Singh",
+    src: "music/Agar Tum Saath Ho - Alka Yagnik, Arijit Singh.m4a",
+    cover: "https://c.saavncdn.com/430/Aashiqui-2-Hindi-2013-500x500.jpg",
   },
-{ 
-  title: "Aankhon Mein Doob Jaane Ko",
-  artist: "Sandesh Shandilya",
-  src: "https://aac.saavncdn.com/730/b7bc5718c8eb6a4afcb7bd1f44d2c9ca_160.mp4",
-  cover: "https://c.saavncdn.com/730/Aankhon-Mein-Doob-Jaane-Ko-Hindi-2024-20240816183310-500x500.jpg" },
-
-  {
-    title:"Husn",
-    artist:"Anuv jain",
-    src:"https://aac.saavncdn.com/436/13795b7aa2e87393366162b9e6a6fe88_160.mp4",
-    cover:"https://c.saavncdn.com/436/Husn-Hindi-2023-20231129054140-500x500.jpg",
-  },
-  {
-    title:"Heeriye",
-    artist:"Arijit Singh",
-    src:"https://aac.saavncdn.com/022/a192e8d320ea5630db314d04fedf0aa5_160.mp4",
-    cover:"https://c.saavncdn.com/022/Heeriye-feat-Arijit-Singh-Hindi-2023-20230928050405-500x500.jpg",
-  },
-  {
-    title:"Choo Lo",
-    artist:"Arijit Singh",
-    src:"https://aac.saavncdn.com/111/09c5dba8ec03665a9a679e19338917a6_160.mp4",
-    cover:"https://c.saavncdn.com/111/Aalas-Ka-Pedh-Hindi-2015-20231005005130-500x500.jpg",
-  },
-  {
-    title:"Agar Tum Saath Ho",
-    artist:"Alka Yagnik,Arijit Singh",
-    src:"music/Agar Tum Saath Ho - Alka Yagnik, Arijit Singh.m4a",
-    cover:"https://c.saavncdn.com/994/Tamasha-Hindi-2015-500x500.jpg",
-  
-    },
-  {
-    title: "ekati ekati ghabarlis na",
-    artist: "Anjali Kulkarni",
-     src: "music/Husn - Anuv Jain.m4a",
-     cover: "https://c.saavncdn.com/875/Chintoo-Marathi-2012-20200122070309-500x500.jpg",
-   },
-   {
-    title: "AAI",
-     artist: "Yogita Koli,Pravin Koli",
-     src: "music/Aai - Pravin Koli, Yogita Koli, Deeya Wadkar, Myra Vaikul, Ankita Raut.m4a",
-     cover: "https://c.saavncdn.com/621/Aai-Marathi-2022-20220505134712-500x500.jpg",
-  },
-  // {
-  //   title: "govyachya kinaryavar",
-  //   artist: "pravin koli",
-  //   src: "music/song3.mp3",
-  //   cover: "music/song33.jpg",},
-
-
+  // Add more songs
 ];
+
+export const englishSongs = [
+  {
+    title: "Shape of You",
+    artist: "Ed Sheeran",
+    src: "music/blue - yung kai.m4a",
+    cover: "https://c.saavncdn.com/399/shades-of-blue-English-2024-20241105054633-500x500.jpg",
+  },
+  // Add more songs
+];
+
+export const marathiSongs = [
+  {
+    title: "Zingaat",
+    artist: "Ajay-Atul",
+    src: "music/Aai - Pravin Koli, Yogita Koli, Deeya Wadkar, Myra Vaikul, Ankita Raut.m4a",
+    cover: "https://c.saavncdn.com/399/shades-of-blue-English-2024-20241105054633-500x500.jpg",
+  },
+  // Add more songs
+];
+
+
+
+
+
+
+
+
+// // songs.js
+// export const songs = [
+//   { 
+//       title: "Jo Tum Mere Ho",
+//     artist: "Anuv Jain",
+//     src: "music/Jo Tum Mere Ho.mp3",
+//     cover: "https://c.saavncdn.com/401/Jo-Tum-Mere-Ho-Hindi-2024-20240731053953-500x500.jpg",
+//      },
+//   {
+//     title: "Apna Bana Le",
+//     artist: "Sachin-Jigar, Arijit Singh, Amitabh Bhattacharya",
+//     src:"https://aac.saavncdn.com/815/483a6e118e8108cbb3e5cd8701674f32_96.mp4",
+//     cover:"https://c.saavncdn.com/815/Bhediya-Hindi-2023-20230927155213-500x500.jpg",
+//   },
+// { 
+//   title: "Aankhon Mein Doob Jaane Ko",
+//   artist: "Sandesh Shandilya",
+//   src: "https://aac.saavncdn.com/730/b7bc5718c8eb6a4afcb7bd1f44d2c9ca_160.mp4",
+//   cover: "https://c.saavncdn.com/730/Aankhon-Mein-Doob-Jaane-Ko-Hindi-2024-20240816183310-500x500.jpg" },
+
+//   {
+//     title:"Husn",
+//     artist:"Anuv jain",
+//     src:"https://aac.saavncdn.com/436/13795b7aa2e87393366162b9e6a6fe88_160.mp4",
+//     cover:"https://c.saavncdn.com/436/Husn-Hindi-2023-20231129054140-500x500.jpg",
+//   },
+//   {
+//     title:"Heeriye",
+//     artist:"Arijit Singh",
+//     src:"https://aac.saavncdn.com/022/a192e8d320ea5630db314d04fedf0aa5_160.mp4",
+//     cover:"https://c.saavncdn.com/022/Heeriye-feat-Arijit-Singh-Hindi-2023-20230928050405-500x500.jpg",
+//   },
+//   {
+//     title:"Choo Lo",
+//     artist:"Arijit Singh",
+//     src:"https://aac.saavncdn.com/111/09c5dba8ec03665a9a679e19338917a6_160.mp4",
+//     cover:"https://c.saavncdn.com/111/Aalas-Ka-Pedh-Hindi-2015-20231005005130-500x500.jpg",
+//   },
+//   {
+//     title:"Agar Tum Saath Ho",
+//     artist:"Alka Yagnik,Arijit Singh",
+//     src:"music/Agar Tum Saath Ho - Alka Yagnik, Arijit Singh.m4a",
+//     cover:"https://c.saavncdn.com/994/Tamasha-Hindi-2015-500x500.jpg",
+  
+//     },
+//   {
+//     title: "ekati ekati ghabarlis na",
+//     artist: "Anjali Kulkarni",
+//      src: "music/Husn - Anuv Jain.m4a",
+//      cover: "https://c.saavncdn.com/875/Chintoo-Marathi-2012-20200122070309-500x500.jpg",
+//    },
+//    {
+//     title: "AAI",
+//      artist: "Yogita Koli,Pravin Koli",
+//      src: "music/Aai - Pravin Koli, Yogita Koli, Deeya Wadkar, Myra Vaikul, Ankita Raut.m4a",
+//      cover: "https://c.saavncdn.com/621/Aai-Marathi-2022-20220505134712-500x500.jpg",
+//   },
+//   {
+//      title: "govyachya kinaryavar",
+//      artist: "pravin koli",
+//     src: "music/song3.mp3",
+//      cover: "music/song33.jpg",},
+
+
+// ];

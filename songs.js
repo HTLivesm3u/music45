ba9366dd95b00a894371502be10a3e5dc91b1285
HTLivesm3u <169054920+HTLivesm3u@users.noreply.github.com>
2024-12-22 17:49:35 +0530

    Update songs.js

--- songs.js
+++ songs.js
@@ -47,16 +47,16 @@ export const hindiSongs = [
     src:"music/Hua Main (From  ANIMAL ) - Raghav Chaitanya, Manoj Muntashir, Pritam.m4a",
     cover:"https://c.saavncdn.com/155/Hua-Main-From-ANIMAL-Hindi-2023-20231011001003-500x500.jpg",
   },
-  {
-    title:'Husn',
-    artist:'Anuv Jain',
-    cover:'https://c.saavncdn.com/436/Husn-Hindi-2023-20231129054140-500x500.jpg',
+{
+    title: 'Husn',
+    artist: 'Anuv Jain',
+    cover: 'https://c.saavncdn.com/436/Husn-Hindi-2023-20231129054140-500x500.jpg',
     src: { 
       high: 'music/Husn - Anuv Jain_high.m4a',
       medium: 'music/Husn - Anuv Jain_medium.m4a',
-      low: 'music/Husn - Anuv Jain_low.m4a',}
-  },
-  
+      low: 'music/Husn - Anuv Jain_low.m4a',
+    },
+},
  ];
 
 export const englishSongs = [

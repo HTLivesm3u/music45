ade4512622b9b0ff3ca44255f3fcaed0a89289e8
HTLivesm3u <169054920+HTLivesm3u@users.noreply.github.com>
2024-12-22 18:10:45 +0530

    Update songs.js

--- songs.js
+++ songs.js
@@ -1,243 +1,249 @@
-// songs.js
 export const hindiSongs = [
-  { 
-   title: "Aankhon Mein Doob Jaane Ko",
-   artist: "Sandesh Shandilya",
-   src: "music/Aankhon Mein Doob Jaane Ko - THE 9TEEN, Sandesh Shandilya.m4a",
- cover: "https://c.saavncdn.com/730/Aankhon-Mein-Doob-Jaane-Ko-Hindi-2024-20240816183310-500x500.jpg" },
- {
-     title:"Agar Tum Saath Ho",
-     artist:"Alka Yagnik,Arijit Singh",
-     src:"music/Agar Tum Saath Ho - Alka Yagnik, Arijit Singh.m4a",
-    cover:"https://c.saavncdn.com/994/Tamasha-Hindi-2015-500x500.jpg",
-     },
-    {
-    title: "Apna Bana Le",
-    artist: "Sachin-Jigar, Arijit Singh, Amitabh Bhattacharya",
-    src:"https://aac.saavncdn.com/815/483a6e118e8108cbb3e5cd8701674f32_96.mp4",
-    cover:"https://c.saavncdn.com/815/Bhediya-Hindi-2023-20230927155213-500x500.jpg",
-  },
-    {
-    title: "Arjan Vailly",
-    artist: "Manan Bhardwaj, Bhupinder Babbal",
-    src:"music/Arjan Vailly (From  ANIMAL ) - Manan Bhardwaj, Bhupinder Babbal.m4a",
-    cover:"https://c.saavncdn.com/092/ANIMAL-Hindi-2023-20231124191036-500x500.jpg",
-  },
   {
-    title:"Chaleya",
-    artist:"Anirudh Ravichander, Arijit Singh, Shilpa Rao, Kumaar",
-    src:"music/Chaleya (From  Jawan ) - Anirudh Ravichander, Arijit Singh, Shilpa Rao, Kumaar.m4a",
-    cover:"https://c.saavncdn.com/026/Chaleya-From-Jawan-Hindi-2023-20230814014337-500x500.jpg",
+    title: "Husn",
+    artist: "Anuv Jain",
+    cover: "https://c.saavncdn.com/436/Husn-Hindi-2023-20231129054140-500x500.jpg",
+    src: {
+      low: "music/Husn - Anuv Jain-low.m4a",
+      medium: "music/Husn - Anuv Jain-medium.m4a",
+      high: "music/Husn - Anuv Jain-high.m4a"
+    }
   },
-  {
-    title:"Choo Lo",
-    artist:"The local Train",
-    src:"music/Choo Lo - The Local Train.m4a",
-    cover:"https://c.saavncdn.com/111/Aalas-Ka-Pedh-Hindi-2015-20231005005130-500x500.jpg",
-  },
-  {
-    title:"Heeriye (feat. Arijit Singh)",
-    artist:"Jasleen Royal, Arijit Singh, Dulquer Salmaan",
-    src:"music/Heeriye (feat. Arijit Singh) - Jasleen Royal, Arijit Singh, Dulquer Salmaan.m4a",
-    cover:"https://c.saavncdn.com/022/Heeriye-feat-Arijit-Singh-Hindi-2023-20230928050405-500x500.jpg",
-  },
-  {
-    title:"Hua Main",
-    artist:"Raghav Chaitanya,Manoj Muntashir,Pritam",
-    src:"music/Hua Main (From  ANIMAL ) - Raghav Chaitanya, Manoj Muntashir, Pritam.m4a",
-    cover:"https://c.saavncdn.com/155/Hua-Main-From-ANIMAL-Hindi-2023-20231011001003-500x500.jpg",
-  },
-{
-    title: 'Husn',
-    artist: 'Anuv Jain',
-    src:"music/Husn - Anuv Jain.m4a",
-    cover: 'https://c.saavncdn.com/436/Husn-Hindi-2023-20231129054140-500x500.jpg',
-},
- {
-  title: "Song Title 6",
-  artist: "Artist Name 6",
-  cover: "cover6.jpg",
-  src: {
-    low: "music/Husn - Anuv Jain-low.m4a",
-    medium: "music/Husn - Anuv Jain-medium.m4a",
-    high: "music/Husn - Anuv Jain-high.m4a"
-  }
-}
- ];
-
-export const englishSongs = [
-  {
-    title: "7 Years",
-    artist: "Lukas Graham",
-    src: "music/7 Years - Lukas Graham.m4a",
-    cover: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQVXQO3G2SrZQQ8Ghxj3i2LY8s4mNYC_XnI0NfYqrYiOdRh2G7lLfBaeH8&s=10",
-  },  
-  {
-    title:"Beautiful Things",
-    artist:"Benson Boone",
-    src:"music/Beautiful Things - Benson Boone.m4a",
-    cover:"https://c.saavncdn.com/751/Beautiful-Things-English-2024-20240404023143-500x500.jpg",
-  },
-  {
-    title: "Blue",
-    artist: "yung kai",
-    src: "music/blue - yung kai.m4a",
-    cover: "https://c.saavncdn.com/399/shades-of-blue-English-2024-20241105054633-500x500.jpg",
-  },
-  {
-    title:"Cheap Thrills",
-    artist:"Sia",
-    src:"music/Cheap Thrills - Sia.m4a",
-    cover:"https://c.saavncdn.com/203/This-Is-Acting-English-2016-500x500.jpg",
-  },
-  {
-    title:"Closer",
-    artist:"Halsey",
-    src:"music/Closer - The Chainsmokers, Halsey.m4a",
-    cover:"https://c.saavncdn.com/147/Closer-English-2016-500x500.jpg",
-  },
-  {
-    title:"Dandelions",
-    artist:"Ruth B",
-    src:"music/Dandelions - Ruth B..m4a",
-    cover:"https://c.saavncdn.com/400/Dandelions-English-2017-500x500.jpg",
-  },
-  {
-    title:"Die With A Smile ",
-    artist:"Lady Gaga, Bruno Mars",
-    src:"music/Die With A Smile - Lady Gaga, Bruno Mars.m4a",
-    cover:"https://c.saavncdn.com/060/Die-With-A-Smile-English-2024-20240816103634-500x500.jpg",
-  },
-  {
-    title:"End of Beginning",
-    artist:"Djo",
-    src:"music/End of Beginning - Djo.m4a",
-    cover:"https://c.saavncdn.com/336/DECIDE-English-2022-20221205093021-500x500.jpg",
-  },
-  {
-    title:"Espresso ",
-    artist:"Sabrina Carpenter",
-    src:"music/Espresso - Sabrina Carpenter.m4a",
-    cover:"https://c.saavncdn.com/111/Espresso-English-2024-20240412064803-500x500.jpg",
-  },
-  {
-    title:"Falling",
-    artist:"Trevor Daniel",
-    src:"music/Falling - Trevor Daniel.m4a",
-    cover:"https://c.saavncdn.com/841/Falling-English-2018-20210703001405-500x500.jpg",
-  },
-  {
-    title:"Heat Waves",
-    artist:"Glass Animals",
-    src:"music/Heat Waves - Glass Animals.m4a",
-    cover:"https://c.saavncdn.com/173/Dreamland-English-2020-20200806000748-500x500.jpg",
-  },
-  {
-    title:"The Stars",
-    artist:"Benson Boone",
-    src:"music/In The Stars - Benson Boone.m4a",
-    cover:"https://c.saavncdn.com/046/In-The-Stars-English-2022-20240404090421-500x500.jpg",
-  },
-  {
-    title:"Lover",
-    artist:"Taylor Swift",
-    src:"music/Lover - Taylor Swift.m4a",
-    cover:"https://c.saavncdn.com/228/Lover-English-2019-20190823031817-500x500.jpg",
-  },
-  {
-    title:"MY EYES",
-    artist:"Travis Scott",
-    src:"music/MY EYES - Travis Scott.m4a",
-    cover:"https://c.saavncdn.com/882/UTOPIA-English-2023-20230728085013-500x500.jpg",
-  },
-  {
-    title:"Night Changes",
-    artist:"One Direction",
-    src:"music/Night Changes - One Direction.m4a",
-    cover:"https://c.saavncdn.com/851/FOUR-Deluxe--English-2014-500x500.jpg",
-  },
-  {
-    title:"STAY (with Justin Bieber)",
-    artist:"The Kid LAROI, Justin Bieber",
-    src:"music/STAY (with Justin Bieber) - The Kid LAROI, Justin Bieber.m4a",
-    cover:"https://c.saavncdn.com/895/Stay-English-2021-20210706223809-500x500.jpg",
-  },
-  {
-    title:"Señorita",
-    artist:"Shawn Mendes, Camila Cabello",
-    src:"music/Señorita - Shawn Mendes, Camila Cabello.m4a",
-    cover:"https://c.saavncdn.com/624/Se-orita-English-2019-20190822022326-500x500.jpg",
-  },
-  {
-    title:"Stereo Hearts (feat. Adam Levine)",
-    artist:"Gym Class Heroes, Adam Levine",
-    src:"music/Stereo Hearts (feat. Adam Levine) - Gym Class Heroes, Adam Levine.m4a",
-    cover:"https://c.saavncdn.com/509/The-Papercut-Chronicles-II-English-2011-20200822053426-500x500.jpg",
-  },
+  // Add more songs here...
 ];
 
-export const marathiSongs = [{
-     title: "AAI",
-      artist: "Yogita Koli,Pravin Koli",
-      src: "music/Aai - Pravin Koli, Yogita Koli, Deeya Wadkar, Myra Vaikul, Ankita Raut.m4a",
-      cover: "https://c.saavncdn.com/621/Aai-Marathi-2022-20220505134712-500x500.jpg",
- },
-  {
-    title:"Jagnayache Deva",
-    artist:"?",
-    src:"music/Aise_Lago_Bhan_Full_Song_Jagnyache_Deva_Song_जगनयच_दव_Marathi.mp3",
-    cover:"https://i.pinimg.com/736x/0d/50/c7/0d50c7d6f608385fadcd964c3651eb92.jpg",
-  },
-  {
-    title:"Baharla Ha Madhumas",
-    artist:"Ajay-Atul, Ajay Gogavale, Shreya Ghoshal",
-    src:"music/Baharla Ha Madhumas - Ajay-Atul, Ajay Gogavale, Shreya Ghoshal.m4a",
-    cover:"https://c.saavncdn.com/989/Maharashtra-Shaheer-Marathi-2023-20230427192041-500x500.jpg",
-  },
 
-  {
-    title:"Chand Tu Nabhatla",
-    artist:"Swapnil Bandodkar",
-    src:"music/Chand Tu Nabhatla - Swapnil Bandodkar.m4a",
-    cover:"https://c.saavncdn.com/329/Sandook-Marathi-2015-500x500.jpg",
-  },
-  {
-    title:"Deva Tujhya Gaabhaaryaalaa",
-    artist:"Adarsh Shinde, Kirti Killedar, Anandi Joshi",
-    src:"music/Deva Tujhya Gaabhaaryaalaa - Adarsh Shinde, Kirti Killedar, Anandi Joshi.m4a",
-    cover:"https://i.scdn.co/image/ab67616d0000aa54d1bc2feb42a7ee92d51d0eee",
-  },
-  {
-    title:"Dewak Kalji Re",
-    artist:"Ajay Gogavale",
-    src:"music/Dewak Kalji Re - Ajay Gogavale.m4a",
-    cover:"https://c.saavncdn.com/461/Redu-Marathi-2017-20180502-500x500.jpg",
-  }, 
-  {
-    title:"Dhaga Dhaga",
-    artist:"harsh wavre, Anandi Joshi",
-    src:"music/Dhaga Dhaga - harsh wavre, Anandi Joshi.m4a",
-    cover:"https://c.saavncdn.com/568/Dagdi-Chawl-Marathi-2015-20190610174048-500x500.jpg",
-  },  
-  {
-    title: "ekati ekati ghabarlis na",
-    artist: "Anjali Kulkarni",
-     src: "music/Ekati Ekati (feat  Shubhankar Kulkarni   Anjali Kulkarni).mp3",
-     cover: "https://c.saavncdn.com/875/Chintoo-Marathi-2012-20200122070309-500x500.jpg",
-   },
-  {
-    title:"Govyachya Kinaryav",
-    artist:"Shubhangii Kedar, Pravin Koli, Rajneesh Patel",
-    src:"music/Govyachya Kinaryav - Shubhangii Kedar, Pravin Koli, Rajneesh Patel.m4a",
-    cover:"https://c.saavncdn.com/122/Govyachya-Kinaryav-Marathi-2020-20210708114432-500x500.jpg",
-  }, 
-  {
-    title:"Gulabachi Kali",
-    artist:"Vaishali Samant, Urmila Dhangar, Amitraj",
-    src:"music/Gulabachi Kali - Vaishali Samant, Urmila Dhangar, Amitraj.m4a",
-    cover:"https://c.saavncdn.com/102/Tu-Hi-Re-Marathi-2015-20241119160647-500x500.jpg",
-  },                   
-];
+// songs.js
+// export const hindiSongs = [
+//   { 
+//    title: "Aankhon Mein Doob Jaane Ko",
+//    artist: "Sandesh Shandilya",
+//    src: "music/Aankhon Mein Doob Jaane Ko - THE 9TEEN, Sandesh Shandilya.m4a",
+//  cover: "https://c.saavncdn.com/730/Aankhon-Mein-Doob-Jaane-Ko-Hindi-2024-20240816183310-500x500.jpg" },
+//  {
+//      title:"Agar Tum Saath Ho",
+//      artist:"Alka Yagnik,Arijit Singh",
+//      src:"music/Agar Tum Saath Ho - Alka Yagnik, Arijit Singh.m4a",
+//     cover:"https://c.saavncdn.com/994/Tamasha-Hindi-2015-500x500.jpg",
+//      },
+//     {
+//     title: "Apna Bana Le",
+//     artist: "Sachin-Jigar, Arijit Singh, Amitabh Bhattacharya",
+//     src:"https://aac.saavncdn.com/815/483a6e118e8108cbb3e5cd8701674f32_96.mp4",
+//     cover:"https://c.saavncdn.com/815/Bhediya-Hindi-2023-20230927155213-500x500.jpg",
+//   },
+//     {
+//     title: "Arjan Vailly",
+//     artist: "Manan Bhardwaj, Bhupinder Babbal",
+//     src:"music/Arjan Vailly (From  ANIMAL ) - Manan Bhardwaj, Bhupinder Babbal.m4a",
+//     cover:"https://c.saavncdn.com/092/ANIMAL-Hindi-2023-20231124191036-500x500.jpg",
+//   },
+//   {
+//     title:"Chaleya",
+//     artist:"Anirudh Ravichander, Arijit Singh, Shilpa Rao, Kumaar",
+//     src:"music/Chaleya (From  Jawan ) - Anirudh Ravichander, Arijit Singh, Shilpa Rao, Kumaar.m4a",
+//     cover:"https://c.saavncdn.com/026/Chaleya-From-Jawan-Hindi-2023-20230814014337-500x500.jpg",
+//   },
+//   {
+//     title:"Choo Lo",
+//     artist:"The local Train",
+//     src:"music/Choo Lo - The Local Train.m4a",
+//     cover:"https://c.saavncdn.com/111/Aalas-Ka-Pedh-Hindi-2015-20231005005130-500x500.jpg",
+//   },
+//   {
+//     title:"Heeriye (feat. Arijit Singh)",
+//     artist:"Jasleen Royal, Arijit Singh, Dulquer Salmaan",
+//     src:"music/Heeriye (feat. Arijit Singh) - Jasleen Royal, Arijit Singh, Dulquer Salmaan.m4a",
+//     cover:"https://c.saavncdn.com/022/Heeriye-feat-Arijit-Singh-Hindi-2023-20230928050405-500x500.jpg",
+//   },
+//   {
+//     title:"Hua Main",
+//     artist:"Raghav Chaitanya,Manoj Muntashir,Pritam",
+//     src:"music/Hua Main (From  ANIMAL ) - Raghav Chaitanya, Manoj Muntashir, Pritam.m4a",
+//     cover:"https://c.saavncdn.com/155/Hua-Main-From-ANIMAL-Hindi-2023-20231011001003-500x500.jpg",
+//   },
+// {
+//     title: 'Husn',
+//     artist: 'Anuv Jain',
+//     src:"music/Husn - Anuv Jain.m4a",
+//     cover: 'https://c.saavncdn.com/436/Husn-Hindi-2023-20231129054140-500x500.jpg',
+// },
+
+//  ];
+
+// export const englishSongs = [
+//   {
+//     title: "7 Years",
+//     artist: "Lukas Graham",
+//     src: "music/7 Years - Lukas Graham.m4a",
+//     cover: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQVXQO3G2SrZQQ8Ghxj3i2LY8s4mNYC_XnI0NfYqrYiOdRh2G7lLfBaeH8&s=10",
+//   },  
+//   {
+//     title:"Beautiful Things",
+//     artist:"Benson Boone",
+//     src:"music/Beautiful Things - Benson Boone.m4a",
+//     cover:"https://c.saavncdn.com/751/Beautiful-Things-English-2024-20240404023143-500x500.jpg",
+//   },
+//   {
+//     title: "Blue",
+//     artist: "yung kai",
+//     src: "music/blue - yung kai.m4a",
+//     cover: "https://c.saavncdn.com/399/shades-of-blue-English-2024-20241105054633-500x500.jpg",
+//   },
+//   {
+//     title:"Cheap Thrills",
+//     artist:"Sia",
+//     src:"music/Cheap Thrills - Sia.m4a",
+//     cover:"https://c.saavncdn.com/203/This-Is-Acting-English-2016-500x500.jpg",
+//   },
+//   {
+//     title:"Closer",
+//     artist:"Halsey",
+//     src:"music/Closer - The Chainsmokers, Halsey.m4a",
+//     cover:"https://c.saavncdn.com/147/Closer-English-2016-500x500.jpg",
+//   },
+//   {
+//     title:"Dandelions",
+//     artist:"Ruth B",
+//     src:"music/Dandelions - Ruth B..m4a",
+//     cover:"https://c.saavncdn.com/400/Dandelions-English-2017-500x500.jpg",
+//   },
+//   {
+//     title:"Die With A Smile ",
+//     artist:"Lady Gaga, Bruno Mars",
+//     src:"music/Die With A Smile - Lady Gaga, Bruno Mars.m4a",
+//     cover:"https://c.saavncdn.com/060/Die-With-A-Smile-English-2024-20240816103634-500x500.jpg",
+//   },
+//   {
+//     title:"End of Beginning",
+//     artist:"Djo",
+//     src:"music/End of Beginning - Djo.m4a",
+//     cover:"https://c.saavncdn.com/336/DECIDE-English-2022-20221205093021-500x500.jpg",
+//   },
+//   {
+//     title:"Espresso ",
+//     artist:"Sabrina Carpenter",
+//     src:"music/Espresso - Sabrina Carpenter.m4a",
+//     cover:"https://c.saavncdn.com/111/Espresso-English-2024-20240412064803-500x500.jpg",
+//   },
+//   {
+//     title:"Falling",
+//     artist:"Trevor Daniel",
+//     src:"music/Falling - Trevor Daniel.m4a",
+//     cover:"https://c.saavncdn.com/841/Falling-English-2018-20210703001405-500x500.jpg",
+//   },
+//   {
+//     title:"Heat Waves",
+//     artist:"Glass Animals",
+//     src:"music/Heat Waves - Glass Animals.m4a",
+//     cover:"https://c.saavncdn.com/173/Dreamland-English-2020-20200806000748-500x500.jpg",
+//   },
+//   {
+//     title:"The Stars",
+//     artist:"Benson Boone",
+//     src:"music/In The Stars - Benson Boone.m4a",
+//     cover:"https://c.saavncdn.com/046/In-The-Stars-English-2022-20240404090421-500x500.jpg",
+//   },
+//   {
+//     title:"Lover",
+//     artist:"Taylor Swift",
+//     src:"music/Lover - Taylor Swift.m4a",
+//     cover:"https://c.saavncdn.com/228/Lover-English-2019-20190823031817-500x500.jpg",
+//   },
+//   {
+//     title:"MY EYES",
+//     artist:"Travis Scott",
+//     src:"music/MY EYES - Travis Scott.m4a",
+//     cover:"https://c.saavncdn.com/882/UTOPIA-English-2023-20230728085013-500x500.jpg",
+//   },
+//   {
+//     title:"Night Changes",
+//     artist:"One Direction",
+//     src:"music/Night Changes - One Direction.m4a",
+//     cover:"https://c.saavncdn.com/851/FOUR-Deluxe--English-2014-500x500.jpg",
+//   },
+//   {
+//     title:"STAY (with Justin Bieber)",
+//     artist:"The Kid LAROI, Justin Bieber",
+//     src:"music/STAY (with Justin Bieber) - The Kid LAROI, Justin Bieber.m4a",
+//     cover:"https://c.saavncdn.com/895/Stay-English-2021-20210706223809-500x500.jpg",
+//   },
+//   {
+//     title:"Señorita",
+//     artist:"Shawn Mendes, Camila Cabello",
+//     src:"music/Señorita - Shawn Mendes, Camila Cabello.m4a",
+//     cover:"https://c.saavncdn.com/624/Se-orita-English-2019-20190822022326-500x500.jpg",
+//   },
+//   {
+//     title:"Stereo Hearts (feat. Adam Levine)",
+//     artist:"Gym Class Heroes, Adam Levine",
+//     src:"music/Stereo Hearts (feat. Adam Levine) - Gym Class Heroes, Adam Levine.m4a",
+//     cover:"https://c.saavncdn.com/509/The-Papercut-Chronicles-II-English-2011-20200822053426-500x500.jpg",
+//   },
+// ];
+
+// export const marathiSongs = [{
+//      title: "AAI",
+//       artist: "Yogita Koli,Pravin Koli",
+//       src: "music/Aai - Pravin Koli, Yogita Koli, Deeya Wadkar, Myra Vaikul, Ankita Raut.m4a",
+//       cover: "https://c.saavncdn.com/621/Aai-Marathi-2022-20220505134712-500x500.jpg",
+//  },
+//   {
+//     title:"Jagnayache Deva",
+//     artist:"?",
+//     src:"music/Aise_Lago_Bhan_Full_Song_Jagnyache_Deva_Song_जगनयच_दव_Marathi.mp3",
+//     cover:"https://i.pinimg.com/736x/0d/50/c7/0d50c7d6f608385fadcd964c3651eb92.jpg",
+//   },
+//   {
+//     title:"Baharla Ha Madhumas",
+//     artist:"Ajay-Atul, Ajay Gogavale, Shreya Ghoshal",
+//     src:"music/Baharla Ha Madhumas - Ajay-Atul, Ajay Gogavale, Shreya Ghoshal.m4a",
+//     cover:"https://c.saavncdn.com/989/Maharashtra-Shaheer-Marathi-2023-20230427192041-500x500.jpg",
+//   },
+
+//   {
+//     title:"Chand Tu Nabhatla",
+//     artist:"Swapnil Bandodkar",
+//     src:"music/Chand Tu Nabhatla - Swapnil Bandodkar.m4a",
+//     cover:"https://c.saavncdn.com/329/Sandook-Marathi-2015-500x500.jpg",
+//   },
+//   {
+//     title:"Deva Tujhya Gaabhaaryaalaa",
+//     artist:"Adarsh Shinde, Kirti Killedar, Anandi Joshi",
+//     src:"music/Deva Tujhya Gaabhaaryaalaa - Adarsh Shinde, Kirti Killedar, Anandi Joshi.m4a",
+//     cover:"https://i.scdn.co/image/ab67616d0000aa54d1bc2feb42a7ee92d51d0eee",
+//   },
+//   {
+//     title:"Dewak Kalji Re",
+//     artist:"Ajay Gogavale",
+//     src:"music/Dewak Kalji Re - Ajay Gogavale.m4a",
+//     cover:"https://c.saavncdn.com/461/Redu-Marathi-2017-20180502-500x500.jpg",
+//   }, 
+//   {
+//     title:"Dhaga Dhaga",
+//     artist:"harsh wavre, Anandi Joshi",
+//     src:"music/Dhaga Dhaga - harsh wavre, Anandi Joshi.m4a",
+//     cover:"https://c.saavncdn.com/568/Dagdi-Chawl-Marathi-2015-20190610174048-500x500.jpg",
+//   },  
+//   {
+//     title: "ekati ekati ghabarlis na",
+//     artist: "Anjali Kulkarni",
+//      src: "music/Ekati Ekati (feat  Shubhankar Kulkarni   Anjali Kulkarni).mp3",
+//      cover: "https://c.saavncdn.com/875/Chintoo-Marathi-2012-20200122070309-500x500.jpg",
+//    },
+//   {
+//     title:"Govyachya Kinaryav",
+//     artist:"Shubhangii Kedar, Pravin Koli, Rajneesh Patel",
+//     src:"music/Govyachya Kinaryav - Shubhangii Kedar, Pravin Koli, Rajneesh Patel.m4a",
+//     cover:"https://c.saavncdn.com/122/Govyachya-Kinaryav-Marathi-2020-20210708114432-500x500.jpg",
+//   }, 
+//   {
+//     title:"Gulabachi Kali",
+//     artist:"Vaishali Samant, Urmila Dhangar, Amitraj",
+//     src:"music/Gulabachi Kali - Vaishali Samant, Urmila Dhangar, Amitraj.m4a",
+//     cover:"https://c.saavncdn.com/102/Tu-Hi-Re-Marathi-2015-20241119160647-500x500.jpg",
+//   },                   
+// ];
 
 
 

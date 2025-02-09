3360416430b9741da56fe0f899e664406b62305d
HTLivesm3u <169054920+HTLivesm3u@users.noreply.github.com>
2025-02-09 22:32:44 +0530

    Update 11main.js

--- 11main.js
+++ 11main.js
@@ -113,8 +113,6 @@ function addEventListeners() {
     });
 }
 
-<<<<<<< HEAD
-
 footerToggleBtn.addEventListener("click", () => {
   if (musicBanner.style.display === "block") {
     musicBanner.style.display = "none"; // Close the music banner
@@ -122,13 +120,12 @@ footerToggleBtn.addEventListener("click", () => {
   } else {
     musicBanner.style.display = "block"; // Open the music banner
     history.pushState({ musicBannerOpen: true }, null, window.location.href); // Update browser history
-=======
-  if (trendingCard) {
+    }  if (trendingCard) {
       trendingCard.addEventListener('click', () => {
           const url = '/index.html'; // Replace with your desired link
           window.open(url,); // Opens in a new tab
       });
->>>>>>> 8ee6dd04b5b46fd2efc3b5cb0bc06ca18d513a18
+
   }
 });
 
